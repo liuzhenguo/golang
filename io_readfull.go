35a26c0752ffa741745881dffa33c173db7f730b
liuzg <695060239@qq.com>
2020-04-22 13:31:19 +0800

    io readfull function

--- io_readfull.go
+++ io_readfull.go
@@ -1,56 +1,22 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io"
-	"net"
+	"os"
 )
 
 func main() {
-	ln, err := net.Listen("tcp", "0.0.0.0:9900")
 
+	buf := make([]byte, 10, 10)
+	buf2 := make([]byte, 10, 10)
+	file, _ := os.Open("test.txt")
+	n, err := io.ReadFull(file, buf)
+	n2, _ := io.ReadFull(file, buf2)
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println(n, n2, err.Error())
+	} else {
+		fmt.Println(string(buf))
+		fmt.Println(string(buf2))
 	}
-	for {
-
-		conn, err := ln.Accept()
-
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-
-		go handle(conn)
-
-	}
-
-}
-func handle(conn net.Conn) {
-	alldatas := make([]byte, 10)
-	rf := bufio.NewReader(conn)
-
-	for {
-		tmpbytes := make([]byte, 16)
-		n, err := io.ReadFull(rf, tmpbytes)
-
-		if err != io.EOF {
-			alldatas = append(alldatas, tmpbytes[:n]...)
-		} else {
-			if err != nil && n == 0 {
-				fmt.Println("no bytes!")
-				break
-			} else {
-				fmt.Println("errror:", err)
-				break
-			}
-		}
-
-	}
-	fmt.Println(string(alldatas))
-
-	conn.Write([]byte("successfully"))
-	defer conn.Close()
 }
