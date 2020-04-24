90e46145c1236d60912fceb88d4a492050c93f4c
liuzg <695060239@qq.com>
2020-04-24 17:00:50 +0800

    这个代码有点绕，需要静静的品品

--- 杂凑代码/func_change_interface.go
+++ 杂凑代码/func_change_interface.go
@@ -1,23 +1,41 @@
+/* 
+	强转成handlerfunc后，赋值给接口，变成了handlerfunc类型，
+	可以调用handlerfunc的方法，又因为本身又是一个函数可以直接
+	调用自己
+
+*/
+
 package main
 
 import (
 	"fmt"
 )
 
+type Handler interface {
+	HandleMessage()
+}
 type HandlerFunc func(a int) error
+
+func (h HandlerFunc) HandleMessage() {
+	fmt.Println("handlemessage!")
+	h(10)
+}
 
 type Handle struct {
 	a int
 }
 
-func (h *Handle) HandlerMessage(b int) error {
-	fmt.Println("b = ", b)
+func (h Handle) handleMsg(a int) error {
+	fmt.Println("a =", a)
 	return nil
 }
-func AddHandler(h HandlerFunc) {
-	h(10)
+
+func AddHandler(h Handler) {
+	h.HandleMessage()
+
 }
+
 func main() {
 	h := Handle{}
-	AddHandler(HandlerFunc(h.HandlerMessage))
+	AddHandler(HandlerFunc(h.handleMsg))
 }
