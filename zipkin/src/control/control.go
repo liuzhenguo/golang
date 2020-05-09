0023004cd19cd17082c4aa8ea6601bf5e659c2b5
liuzg <695060239@qq.com>
2020-05-09 17:14:08 +0800

    修改代码，对想要追踪的地方使用span追踪

--- zipkin/src/control/control.go
+++ zipkin/src/control/control.go
@@ -48,16 +48,15 @@ func invoall() http.HandlerFunc {
 }
 func invohandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//return func(w http.ResponseWriter, r *http.Request) {
-		//多个请求超时控制并发请求
+
 		var wg sync.WaitGroup
+
 		fmt.Println("调用开始......")
 		//设置超时时间
 		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*time.Duration(envbuild.GetTimeout()))
-		span := tracer.StartSpan("dbspan")
-		defer span.Finish()
+
 		//将span放在ctx中
-		ctx = zipkin.NewContext(ctx, span)
+		//ctx = zipkin.NewContext(ctx, span)
 
 		//获取数据库的数据
 		wg.Add(1)
@@ -70,20 +69,41 @@ func invohandler() http.HandlerFunc {
 			GetRedisData(ctx)
 			defer wg.Done()
 		}()
-		// wg.Add(1)
-		// go func() {
-		// 	GetRpcData(ctx)
-		// 	defer wg.Done()
-		// }()
+		go func() {
+			nextfunc(w, r)
+		}()
+		go func() {
+			nextone(w, r)
+		}()
 		defer cancel()
 		wg.Wait()
 		w.Write([]byte("successfully"))
 	}
 
 }
+func nextfunc(w http.ResponseWriter, r *http.Request) {
+
+	span, _ := tracer.StartSpanFromContext(r.Context(), "nextfunc")
+
+	span.Tag("nextfunc111", "sleep 100 ms")
+
+	defer span.Finish()
+	time.Sleep(time.Millisecond * 100)
+	span.Annotate(time.Now(), "expensive_call_func")
+}
+func nextone(w http.ResponseWriter, r *http.Request) {
+
+	span, _ := tracer.StartSpanFromContext(r.Context(), "nextone")
+	span.Tag("nextone", "sleep 150 ms")
+	defer span.Finish()
+
+	time.Sleep(time.Millisecond * 150)
+	span.Annotate(time.Now(), "expensive_call_nexone_end")
+
+}
 func Start() {
 
-	tracer = gettracer("zipkin_trace_081", "0.0.0.0:8899")
+	tracer = gettracer("zipkin_trace_101", "0.0.0.0:8899")
 	serverMiddleware := zipkinhttp.NewServerMiddleware(
 		tracer, zipkinhttp.TagResponseSize(true),
 	)
