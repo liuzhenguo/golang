b12371f3f32de03111d4cbb5468f56b70ad70e8a
liuzg <695060239@qq.com>
2020-05-22 09:28:53 +0800

    对比这两段代码

---
+++ 杂凑代码/chantest2.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+
+	"time"
+)
+
+type Counter struct {
+	count int
+}
+
+var mapChan = make(chan map[string]*Counter, 1)
+
+func (counter *Counter) String() string {
+	return fmt.Sprintf("{count:%d}", counter.count)
+}
+func main() {
+
+	sycChan := make(chan struct{}, 2)
+
+	go func() {
+
+		for {
+
+			if elem, ok := <-mapChan; ok {
+
+				counter := elem["count"]
+				counter.count++
+
+			} else {
+				break
+			}
+
+		}
+		fmt.Println("Stopped.[reciver]")
+		sycChan <- struct{}{}
+
+	}()
+
+	go func() {
+
+		countMap := map[string]*Counter{
+			"count": &Counter{},
+		}
+		for i := 0; i < 5; i++ {
+
+			mapChan <- countMap
+			time.Sleep(time.Millisecond)
+			fmt.Printf("The count map:%v,[send]\n", countMap)
+		}
+		close(mapChan)
+		sycChan <- struct{}{}
+
+	}()
+	<-sycChan
+	<-sycChan
+}
